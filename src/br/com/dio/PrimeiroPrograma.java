f5a56ff371142f40a01e15bc1942615071013bb4
Biancaareiis <biancaassumpcao75@gmail.com>
2022-05-22 20:40:08 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,10 +7,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato();
-		Livros livros = new Livros();
-		
 		System.out.println(gato);
-		System.out.println(livros);
 		
 		/*int a = 2;
 		int b = 3;
